23bbcc2decf4982b71e198b4121ad294e153bedc
tomo520473 <hasep.ss@gmail.com>
2024-07-01 20:33:19 +0900

    2-12 operator

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,18 @@ console.log(piyo);
 
 piyo = "piyopiyo";
 console.log(piyo);
+
+const num1 = 3;
+const num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+const num3 = 10;
+console.log(num1 + num2 * num3);
+
