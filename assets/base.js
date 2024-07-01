866d5ef94407a43001aded62a72c412e26536f64
tomo520473 <hasep.ss@gmail.com>
2024-07-01 20:35:01 +0900

    2-12 initial

---
+++ assets/base.js
@@ -0,0 +1,6 @@
+const currentResultOutput = document.getElementById("current-result");
+
+function outputResult(result) {
+  console.log(currentResultOutput);
+  currentResultOutput.innerHTML = result;
+}
--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,4 @@
-console.log("Hello world!");
-
-const hoge = "hello world";
-console.log(hoge);
-
-//hoge = "piyo";
-//console.log(hoge);
-
-let piyo = "piyo!";
-console.log(piyo);
-
-piyo = "piyopiyo";
-console.log(piyo);
-
 const num1 = 3;
 const num2 = 5;
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
-
-console.log(num1 % num2);
-console.log(num1 ** num2);
-
-const num3 = 10;
-console.log(num1 + num2 * num3);
-
+// outputResult(num1 + num2);
